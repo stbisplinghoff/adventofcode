0c630217f76e0915e8e9086983c86c884b1f6958
Stefan Bisplinghoff <stefan_github@mail.bisplinghoff.de>
2021-11-17 21:53:06 +0100

    Solution for day 6

---
+++ 2020/day_6/day6.py
@@ -0,0 +1,20 @@
+# Solution to Advent of Code 2020 day 6
+import math
+
+# Read data
+with open("input.txt") as inFile:
+    groups = inFile.read().split("\n\n")
+
+# Part 1
+yesAnswers = sum([len(set(group.replace("\n", ""))) for group in groups])
+print("Solution for part 1:", yesAnswers)
+
+# Part 2
+yesAnswers = 0
+for group in groups:
+    persons = group.split("\n")
+    sameAnswers = set(persons[0])
+    for person in persons[1:]:
+        sameAnswers &= set(person)
+    yesAnswers += len(sameAnswers)
+print("Solution for part 2:", yesAnswers)
