5d7f8e77c1e54e76eca58ee5e1ab26c5d406a301
Stefan Bisplinghoff <stefan_github@mail.bisplinghoff.de>
2021-12-07 08:42:40 +0100

    Solution for day 6

---
+++ 2021/day_06/day_06.py
@@ -0,0 +1,29 @@
+# Solution to Advent of Code 2021 day 6
+import numpy
+
+# Read input
+with open("input.txt") as inFile:
+    initialFishState = [int(age) for age in inFile.read().split(",")]
+
+## Part 1
+# Simulate every fish in a numpy array
+fish = numpy.array(initialFishState, dtype=int)
+for day in range(80):
+    newFish = fish.size - numpy.count_nonzero(fish)
+    fish -= 1
+    fish = numpy.append(numpy.where(fish == -1, 6, fish), [8] * newFish)
+print("Solution for part 1:", fish.size)
+
+## Part 2
+# Different approach, as numbers will get too large for handling an array element for each fish
+# Do not store every fish but only number of fish per age
+fishPopulation = {age: 0 for age in range(9)}
+for fish in initialFishState:
+    fishPopulation[fish] += 1
+for day in range(256):
+    newFish = fishPopulation[0]
+    for age in range(8):
+        fishPopulation[age] = fishPopulation[age+1]
+    fishPopulation[6] += newFish
+    fishPopulation[8] = newFish
+print("Solution for part 2:", sum(fishPopulation.values()))
